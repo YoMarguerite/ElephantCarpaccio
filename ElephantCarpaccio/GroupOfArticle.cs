334bd51b9cf7a04ef56086e769252328751df1aa
Yoann Marguerite <yoann.marguerite@orange.fr>
2021-03-25 14:23:55 +0100

    Class pour grouper les articles

---
+++ ElephantCarpaccio/GroupOfArticle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElephantCarpaccio
+{
+    public class GroupOfArticle
+    {
+        public Article article;
+        public int quantity;
+
+        public GroupOfArticle(Article _article, int _quantity)
+        {
+            article = _article;
+            quantity = _quantity;
+        }
+
+        public string Quantity
+        {
+            get { return Formatter.FormatInt(quantity); }
+        }
+
+        public float TotalPrice
+        {
+            get { return article.unitPrice * quantity; }
+        }
+
+        public string TotalPriceFormat
+        {
+            get { return Formatter.FormatPrice(article.unitPrice * quantity); }
+        }
+
+        public void Display()
+        {
+            if (quantity > 0)
+            {
+                Console.WriteLine($"{article.Name} --- {Quantity} --- {article.Price} --- {TotalPriceFormat}");
+            }
+        }
+    }
+}
