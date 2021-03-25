1c73dbb8653b2282f7152e62421915df4db9a0c1
Yoann Marguerite <yoann.marguerite@orange.fr>
2021-03-25 13:59:10 +0100

    Ajoutez des fichiers projet.

---
+++ ElephantCarpaccio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElephantCarpaccio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
