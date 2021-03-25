a8b2563a092b75a45e0a8d093d8685762374517e
Yoann Marguerite <yoann.marguerite@orange.fr>
2021-03-25 14:18:55 +0100

    Première version pour la classe Article

---
+++ ElephantCarpaccio/Article.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElephantCarpaccio
+{
+    public class Article
+    {
+        public string name;
+        public float unitPrice;
+
+        public Article(string _name, float _unitPrice)
+        {
+            name = _name;
+            unitPrice = _unitPrice;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return Formatter.FormatString(name);
+            }
+        }
+
+        public string Price
+        {
+            get { return Formatter.FormatPrice(unitPrice); }
+        }
+    }
+}
